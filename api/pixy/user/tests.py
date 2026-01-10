e7e1b5c0ae9559b7c37d32424b61130138ffe9a1
melosamuel <merlosamuel@protonmail.ch>
2026-01-10 11:09:49 -0300

    test: refresh token with valid refersh

--- api/pixy/user/tests.py
+++ api/pixy/user/tests.py
@@ -65,7 +65,44 @@ class JWTAuthenticationTest(APITestCase):
         )
 
     def test_refresh_token_with_valid_refresh(self):
-        pass
+        url = reverse('token_obtain_pair')
+        payload = {
+            "email": "test@example.com",
+            "password": "testpass123"
+        }
+        res = self.client.post(
+            url,
+            payload,
+            format='json'
+        )
+
+        self.assertEqual(
+            res.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertIn(
+            'access',
+            res.data
+        )
+        self.assertIn(
+            'refresh',
+            res.data
+        )
+
+        refresh_token = res.data['refresh']
+
+        refresh_url = reverse('token_refresh')
+        refresh_payload = {
+            "refresh": refresh_token
+        }
+        res = self.client.post(
+            refresh_url,
+            refresh_payload,
+            format='json'
+        )
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('access', res.data)
 
     def test_refresh_token_with_invalid_refresh(self):
         pass
